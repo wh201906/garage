5000bbabb267f126ef640021b025012bbcc60193
wh201906 <wh201906@yandex.com>
2021-06-25 14:57:43 +0800

    SysY: Store tokens in a vector

--- Misc/SysY/scannerGen.sh
+++ Misc/SysY/scannerGen.sh
@@ -1,7 +1,7 @@
 flex SysY.l
 if [ $? -ne 0 ]; then exit 1; fi
 echo "flex completed"
-gcc -g lex.yy.c -o flexx
+g++ -g lex.yy.c -o flexx
 if [ $? -ne 0 ]; then exit 1; fi
 echo "compile completed"
 chmod u+x ./flexx
