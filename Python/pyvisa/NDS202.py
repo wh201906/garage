1977c116a91b46620e049ef7fab915583145e202
wh201906 <wh201906@yandex.com>
2021-09-09 01:09:23 +0800

    NDS202: Fetch data in the screen

--- Python/pyvisa/NDS202.py
+++ Python/pyvisa/NDS202.py
@@ -22,6 +22,7 @@ if (cmd == 'q' or cmd == 'e' or cmd == 'quit' or cmd == 'exit'):
 device = rm.open_resource("TCPIP::" + ip + "::" + port + "::SOCKET")
 device.write_termination = '\n'
 device.read_termination = '\n'
+device.timeout = 4000
 
 info = device.query("*IDN?")
 print(info)
@@ -31,6 +32,21 @@ if info.find(deviceName) == -1:
     device.close()
     exit()
 
-device.write(":AUTOset ON")
+# device.write(":AUTOset ON")
+
+device.write(":DATA:WAVE:SCREen:HEAD?")
+len = int.from_bytes(device.read_bytes(4), byteorder='little')
+print(len)
+info = str(device.read_bytes(len))
+print(info)
+
+device.write(":DATA:WAVE:SCREen:CH1?")
+len = int.from_bytes(device.read_bytes(4), byteorder='little')
+print(len)
+data = device.read_bytes(len)
+lis = []
+for i in range(0, len, 2):
+    lis.append(int.from_bytes(data[i:i + 2], byteorder='little', signed=True))
+print(lis)
 
 device.close()
