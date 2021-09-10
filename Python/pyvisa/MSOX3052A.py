032be7957369a337f4c8ff66af0720164023847f
wh201906 <wh201906@yandex.com>
2021-09-10 21:00:47 +0800

    MSOX3052A: Scan
    around 50ms per point

--- Python/pyvisa/MSOX3052A.py
+++ Python/pyvisa/MSOX3052A.py
@@ -1,11 +1,24 @@
 import pyvisa
 from visadev import VisaDev
+from time import time, sleep
 
 
 class MSOX3052A(VisaDev):
     def __init__(self, resource):
         super(MSOX3052A, self).__init__(resource)
         self.name = "3052A"
+
+    def gen_getFreq(self):
+        return float(device.query(":WGEN:FREQuency?"))
+
+    def gen_setFreq(self, freq, waitFinished=True):
+        device.write(":WGEN:FREQuency " + str(freq))
+        if waitFinished:
+            return device.query("*OPC?")
+
+    def measure_Vpp(self, ch=1):
+        device.write(":MEASure:VPP CHANnel" + str(ch))
+        return float(device.query(":MEASure:VPP?"))
 
 
 if __name__ == '__main__':
@@ -21,6 +34,16 @@ if __name__ == '__main__':
     else:
         print(device.getName() + " connected")
 
-    device.write(':AUToscale')
+    # device.write(':AUToscale')
+    print(device.gen_getFreq())
+
+    t1 = time()
+    #~50ms per cycle
+    for i in range(10700000, 10900000, 1000):
+        device.gen_setFreq(i)
+        print(device.measure_Vpp(1))
+        # print(device.gen_getFreq())
+    t2 = time()
+    print(t2 - t1)
 
     device.close()
