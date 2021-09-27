10cf9e7b362d3f887e747d4e659267e4cf82387e
wh201906 <wh201906@yandex.com>
2021-09-27 13:17:25 +0800

    DSA815: Get trace

--- Python/pyvisa/DSA815.py
+++ Python/pyvisa/DSA815.py
@@ -1,5 +1,6 @@
 import pyvisa
 from visadev import VisaDev
+from struct import unpack
 
 
 class DSA815(VisaDev):
@@ -10,9 +11,23 @@ class DSA815(VisaDev):
     def setCenterFreq(self, freq):
         self.write(":FREQ:CENT " + str(freq))
 
+    def trace_get(self):
+        byteorder = device.query(":FORM:BORD?").lower()
+        if "norm" in byteorder:
+            byteorder = "<"
+        else:
+            byteorder = ">"
+        device.write(":FORM REAL")
+        device.write(":TRAC? TRACE1")
+        len = device.read_bytes(11)
+        len = int(len[-9:])
+        data = device.read_bytes(len)
+        fmt = byteorder + "f" * (len // 4)
+        return unpack(fmt, data)
+
 
 if __name__ == '__main__':
-    res = VisaDev.openProc()
+    res = VisaDev.openProc(1)
     if res is None:
         exit(0)
     device = DSA815(res)
@@ -25,5 +40,6 @@ if __name__ == '__main__':
         print(device.getName() + " connected")
 
     device.setCenterFreq(1000000)
+    print(device.trace_get())
 
     device.close()
