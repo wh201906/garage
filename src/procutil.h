278be07ec2f4350282b22775f54139e72b7787e5
wh201906 <wh201906@yandex.com>
2022-03-09 15:34:02 +0800

    First commit
    Finish UI
    Add class ProcUtil

---
+++ src/procutil.h
@@ -0,0 +1,24 @@
+﻿#ifndef PROCUTIL_H
+#define PROCUTIL_H
+
+#include <QObject>
+#include <windows.h>
+
+// migrated from
+// https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+class ProcUtil : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ProcUtil(QObject *parent = nullptr);
+
+    static BOOL GetProcessList();
+    static BOOL ListProcessModules(DWORD dwPID);
+    static BOOL ListProcessThreads(DWORD dwOwnerPID);
+    static void printError(TCHAR* msg);
+
+signals:
+
+};
+
+#endif // PROCUTIL_H
